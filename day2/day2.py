754f7b3dbaad9e27aa8e1bccab4cc5fc8c091746
Jason Kuo <jasonkuo147@gmail.com>
2025-12-02 00:53:13 -0500

    Day 2 refactor

--- day2/day2.py
+++ day2/day2.py
@@ -14,19 +14,13 @@ for line in lines:
     id1, id2 = map(int, line.split("-"))
     for i in range(id1, id2+1):
         id = str(i)
-        id_good = False
         for j in range(1, len(id)//2+1):
             if len(id) % j != 0:
                 continue
-            good = True
-            for k in range(j, len(id), j):
-                if id[:j] != id[k:k+j]:
-                    good = False
-                    break
-            if good:
-                id_good = True
+            if id[0] != id[j]:
+                continue
+            if id == (id[:j] * (len(id)//j)):
+                ans += i
                 break
-        if id_good:
-            ans += i
 
 print(ans)
