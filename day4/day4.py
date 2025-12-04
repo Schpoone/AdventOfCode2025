c0ed0b0216b07fde785ec1b7028b96c087ddfdb2
Jason Kuo <jasonkuo147@gmail.com>
2025-12-04 00:20:43 -0500

    Day 4 refactor

--- day4/day4.py
+++ day4/day4.py
@@ -17,16 +17,13 @@ for y, line in enumerate(lines):
         if c == '@':
             grid.add((x,y))
 
-new_grid = set(grid)
-keep_going = True
-while keep_going:
-    keep_going = False
-    for x, y in grid:
+new_grid: set[tuple[int, int]] = set()
+while len(new_grid) != len(grid):
+    new_grid = set(grid)
+    for x, y in new_grid:
         adj = [(x+1,y),(x+1,y+1),(x,y+1),(x-1,y),(x-1,y-1),(x-1,y+1),(x+1,y-1),(x,y-1)]
-        if len([p for p in adj if p in grid]) < 4:
+        if len([p for p in adj if p in new_grid]) < 4:
             ans += 1
-            new_grid.remove((x,y))
-            keep_going = True
-    grid = set(new_grid)
+            grid.remove((x,y))
 
 print(ans)
