7efc2a0e44376289b1e2deedbfcffba91e6c7691
Jason Kuo <jasonkuo147@gmail.com>
2025-12-06 01:10:53 -0500

    Day 6: add back part 1

--- day6/day6.py
+++ day6/day6.py
@@ -8,6 +8,25 @@ lines = None
 with open(filename, "r") as f:
     lines = f.readlines()
 
+# Part 1
+# opts = lines[-1].split()
+# cols = list()
+# for op in opts:
+#     if op == "+":
+#         cols.append(0)
+#     else:
+#         cols.append(1)
+#
+# for line in lines[:-1]:
+#     for idx, num in enumerate(line.split()):
+#         if opts[idx] == "+":
+#             cols[idx] += int(num)
+#         else:
+#             cols[idx] *= int(num)
+# print(sum(cols))
+
+
+# Part 2
 ans = 0
 
 opts: list[str] = list()
@@ -23,6 +42,21 @@ for c in lines[-1].strip("\n"):
         widths.append(0)
 widths[-1] += 1
 
+"""
+Convert the characters in the columns
+e.g.
+
+123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  
+
+      |
+      v
+
+[['1', '24', '356'], ['369', '248', '8'], ['32', '581', '175'], ['623', '431', '4']]
+
+"""
 digits: list[list[str]] = list(list("" for _ in range(widths[idx])) for idx in range(len(widths))) # list of the column sections which are lists of the digit columns
 for line in lines[:-1]:
     col_idx = 0
